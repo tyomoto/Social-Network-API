779363a954c9ee15cf6bf01fdc7334123edfb8b2
T Omoto <tyler.omoto@gmail.com>
2022-07-19 02:20:24 -0400

    added in thought routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const thoughtRoutes = require('./thoughtRoutes');
+const thoughtRoute = require('./thoughtRoute');
 
 router.use('/users', userRoutes);
-router.use('/thoughts', thoughtRoutes);
+router.use('/thoughts', thoughtRoute);
 
 module.exports = router;
