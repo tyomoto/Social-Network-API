c690d2e6cb5be31a1316f50bf3a9e272c7864fa8
T Omoto <tyler.omoto@gmail.com>
2022-07-19 02:05:32 -0400

    added user routes

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,27 @@
+const router = require('express').Router();
+
+const {
+    getAllUsers,
+    getSingleUser,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    deleteFriend
+} = require('../../controllers/userController')
+
+// /api/users
+router.route('/').get(getAllUsers).post(createUser);
+
+// /api/users/:userId
+router.route('/:userId')
+.get(getSingleUser)
+.put(updateUser)
+.delete(deleteUser);
+
+// /api/users/:userId/friends/:friendId
+router.route('/:userId/friends/:friendId')
+.put(addFriend)
+.delete(deleteFriend)
+
+module.exports = router;
